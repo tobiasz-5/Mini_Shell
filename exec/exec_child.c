6b066ee4463b6b7e7f6393eac92e249ce3110944
Tobiasz <schettinitobia@gmail.com>
2024-11-27 20:54:35 +0100

    upd

--- exec/exec_child.c
+++ exec/exec_child.c
@@ -6,12 +6,18 @@
 /*   By: tschetti <tschetti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 10:08:03 by tschetti          #+#    #+#             */
-/*   Updated: 2024/11/27 10:08:05 by tschetti         ###   ########.fr       */
+/*   Updated: 2024/11/27 20:52:20 by tschetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../miniheader.h"
 
+/*
+finalizza redir input
+-se last_input_fd e' stato usato/aperto
+duplica, con dup2, l'fd su stdin
+-poi chiude l'fd perche non serve piu e va chiuso
+*/
 void	finalize_input_redirection(int last_input_fd)
 {
 	if (last_input_fd != -1)
@@ -25,6 +31,13 @@ void	finalize_input_redirection(int last_input_fd)
 	}
 }
 
+/*
+apre file in sola lettura
+se open fallisce (val < 0), chiama perror e ritorna -999 al chiamante
+process_input_redirection, se open e' ok la f restituisce
+il ritorno di open(cioe' valore intero >= a zero [0,1 e 2 sono riservati
+rispettiv. a stdin,stdout, stderr])
+*/
 int	open_input_file(const char *filename)
 {
 	int	fd;
@@ -38,6 +51,21 @@ int	open_input_file(const char *filename)
 	return (fd);
 }
 
+/*
+processa redir di input
+-chiude file di input se e' stato aperto 
+in precedenza-> valore diverso da -1(val di default)
+-gestisce sia redir in che heredoc:
+se redir in usa direttamente filename se heredoc
+usa nome file heredoc tmp generato prima /tmp/heredoc_ ecc.
+-apre l'fd last_input_fd(solo lettura)
+-se errore di apertura (-999 err custom) ritorna -999
+e lo assegna a last_input_fd, quindi se c'e' un errore
+nell open -> last_input_fd e' -999 [fd validi possono essere solo 0 o positivi]
+(e quindi ritorna 'errore' return 0)
+altrimenti open_input_file assegna a last_input_fd il ritorno di open
+e questa f ritorna 1->ok
+*/
 int	process_input_redirection(t_redirection *redirection,
 			int *last_input_fd)
 {
@@ -65,6 +93,10 @@ int	process_input_redirection(t_redirection *redirection,
 	return (1);
 }
 
+/*
+processa redir di input primo if
+o altrimenti redir di output secondo if
+*/
 int	process_redirection(t_redirection *redirection,
 			int *last_input_fd, t_io_fds *fds, t_shell_state *shell_state)
 {
@@ -85,6 +117,13 @@ int	process_redirection(t_redirection *redirection,
 	return (1);
 }
 
+/*
+gestisce le redir nel figlio
+-inizializza last_input_fd a -1
+-cicla su tutte le redir del comando
+-processa i vari tipi di redir
+-finalizza la redir di input
+*/
 int	handle_child_redirections(t_command *command,
 			t_io_fds *fds, t_shell_state *shell_state)
 {
--- exec/exec_init_args.c
+++ exec/exec_init_args.c
@@ -6,12 +6,19 @@
 /*   By: tschetti <tschetti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 10:07:28 by tschetti          #+#    #+#             */
-/*   Updated: 2024/11/27 10:07:30 by tschetti         ###   ########.fr       */
+/*   Updated: 2024/11/27 19:23:46 by tschetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../miniheader.h"
 
+/*
+inizializza il comando, chiamando prepare arguments
+chiamante e' execute_single_command in exec.c
+char ***args_array, passimo puntatore a doppio puntatore a char
+bool **args_quote_flags, passiamo puntatore a puntatore a bool
+[per modificare senza ritornare]
+*/
 int	initialize_command_args(t_command *command, char ***args_array,
 				bool **args_quote_flags)
 {
@@ -20,6 +27,15 @@ int	initialize_command_args(t_command *command, char ***args_array,
 	return (0);
 }
 
+/*
+prepara gli argomenti
+converte lista di argomenti in un array di stringhe**
+[--passiamo triplo puntatore per aggiornare direttament puntatore
+al doppio puntatore senza dover ritornarlo (aggiorna dinamicamente
+array di stringhe, cosi modifiche restano tali anche fuori dalla f)
+-> passaggio per riferimento, la f modifica direttamente l'array
+per il chiamante, senza ritornarlo]
+*/
 int	prepare_arguments(t_command *command, char ***args_array,
 				bool **args_quote_flags)
 {
